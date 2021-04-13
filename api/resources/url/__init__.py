b6355fc3bb097554012700557968c7d9a1b9a64e
Jonas Duarte <duarte.jsystem@gmail.com>
2021-04-13 14:02:32 -0300

    Featured: Validação e mensagem de erro de estrutura da URL

--- api/resources/url/__init__.py
+++ api/resources/url/__init__.py
@@ -3,7 +3,7 @@ from flask_restful import Resource
 from flask_api_cache import ApiCache
 from werkzeug.exceptions import NotFound
 from modules.db.mysql import Database
-from commons import validate_short_url, new_short_url
+from commons import validate_url, validate_short_url, new_short_url
 from commons.errors import DuplicatedEntryError
 
 from os import path
@@ -48,50 +48,58 @@ class Url(Resource):
         
     
     def post(self):
-        data = request.get_json()
-        
-        parameters = {
-                'name': data['URLName'],
-                'requested_name': data.get('requestedName'),
-            }
-        
-        if data.get('dateOfExpire'):
-            parameters['column_expire_at'] = ', expire_at'
-            parameters['value_expire_at'] = f', \'{data.get("dateOfExpire")}\''
-        else:
-            parameters['column_expire_at'] = ''
-            parameters['value_expire_at'] = ''
-        
-        while True:
-            try:
-                if not data.get('requestedName'):
-                    parameters['short_name'] = new_short_url(data['URLName'])
-                else:
-                    parameters['short_name'] = '-'.join(data['requestedName'].lower().split(' '))
+        try:
+            data = request.get_json()
             
-                query = Database().parameters_parse(
-                    path.join(
-                        'api', 'commons', 'queries', 'new_short_url.sql'
-                    ),parameters
-                )
-                Database().execute_with_commit(query)
-
-            except DuplicatedEntryError as e:
-                if 'for key \'name\'' in str(e):
-                    shortUrl, expireAt = Database().execute_with_return(
-                        f'select short_name, expire_at from urls where name=\'{data["URLName"]}\''
-                    )[0]
-                    
-                    return {
-                        'response': 'Esta URL já possui uma versão encurtada!',
-                        'shortUrl': shortUrl,
-                        'expireAt': datetime.strftime(expireAt, '%d-%m-%Y %H:%M:%S')
-                    }, 303
-                    
-                data['requestedName'] = None
+            validate_url(data['URLName'])
+            
+            parameters = {
+                    'name': data['URLName'],
+                    'requested_name': data.get('requestedName'),
+                }
+            
+            if data.get('dateOfExpire'):
+                parameters['column_expire_at'] = ', expire_at'
+                parameters['value_expire_at'] = f', \'{data.get("dateOfExpire")}\''
             else:
-                break
+                parameters['column_expire_at'] = ''
+                parameters['value_expire_at'] = ''
             
+            while True:
+                try:
+                    if not data.get('requestedName'):
+                        parameters['short_name'] = new_short_url(data['URLName'])
+                    else:
+                        parameters['short_name'] = '-'.join(data['requestedName'].lower().split(' '))
+                
+                    query = Database().parameters_parse(
+                        path.join(
+                            'api', 'commons', 'queries', 'new_short_url.sql'
+                        ),parameters
+                    )
+                    Database().execute_with_commit(query)
+
+                except DuplicatedEntryError as e:
+                    if 'for key \'name\'' in str(e):
+                        shortUrl, expireAt = Database().execute_with_return(
+                            f'select short_name, expire_at from urls where name=\'{data["URLName"]}\''
+                        )[0]
+                        
+                        return {
+                            'response': 'Esta URL já possui uma versão encurtada!',
+                            'shortUrl': shortUrl,
+                            'expireAt': datetime.strftime(expireAt, '%d-%m-%Y %H:%M:%S')
+                        }, 303
+                        
+                    data['requestedName'] = None
+                    
+                else:
+                    break
+        except AssertionError:
+            return {
+                'response': 'Verifique os parâmetros!'
+            }, 401
+                
         return {
             'response': 'Endpoint criado com sucesso',
             'shortURL': parameters['short_name']
