1314b4cd584a3d39372af65ae8f4475a27379f6b
Jonas Duarte <duarte.jsystem@gmail.com>
2021-04-13 13:53:58 -0300

    Changed: A data atual é adquirida na própria instrução SQL.

--- api/cron.py
+++ api/cron.py
@@ -4,8 +4,7 @@ from modules.db.mysql import Database
 
 while True:
     try:
-        thisMoment = datetime.strftime(datetime.now(), '%d-%m-%Y %H:%M:%S')
-        Database().execute_with_commit(f'delete from urls where expire_at <= \'{thisMoment}\'')
-        sleep(3600)
+        Database().execute_with_commit(f'delete from urls where expire_at <= NOW()')
+        sleep(10)
     except Exception as e:
         print(e)
